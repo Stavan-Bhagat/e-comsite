5e441df5235a33d29b35971bc34f1fb6c1cd4945
Stavan Bhagat <bhagatstavan7@gmail.com>
2024-06-17 10:37:43 +0530

    f

--- backend/socket.js
+++ backend/socket.js
@@ -45,35 +45,3 @@ module.exports = {
     return io;
   }
 };
-
-// let io;
-// const {
-//   MSG_SOCKET_IO_NOT_INITIALIZED,
-//   MSG_SERVER_INSTANCE_REQUIRED
-// } = require('./constant/errorMessage.constant');
-
-// module.exports = {
-//   init: (server) => {
-//     if (!server) {
-//       throw new Error(MSG_SERVER_INSTANCE_REQUIRED);
-//     }
-
-//     const socketIo = require('socket.io')(server, {
-//       cors: {
-//         origin: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
-//         methods: ['GET', 'POST']
-//       }
-//     });
-
-//     io = socketIo;
-//     console.log('Socket.IO initialized');
-//     return io;
-//   },
-
-//   getIo: () => {
-//     if (!io) {
-//       throw new Error(MSG_SOCKET_IO_NOT_INITIALIZED);
-//     }
-//     return io;
-//   }
-// };
